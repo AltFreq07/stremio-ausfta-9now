55fc1d1ad51ff8e1f9ff6d8f5c8d4007bb96a090
altfreq07 <altfreq07@gmail.com>
2023-09-25 21:07:31 +1000

    Auto-commit

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ serveHTTP(addonInterface, { port: process.env.PORT || 64414 });
 
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
-// for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
+// for more information on deploying, see: https://github.com/Stremnpm install beamup-cli -gio/stremio-addon-sdk/blob/master/docs/deploying/README.md
